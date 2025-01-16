6a61f250a13c2ffe03ecbcca8fcfee96b05756db
EvaBytes <evasevillanop@gmail.com>
2025-01-16 11:19:06 +0100

    Table Update

--- src/assets/BookingStyles.js
+++ src/assets/BookingStyles.js
@@ -4,32 +4,38 @@ export const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
   margin: 20px 0;
-  font-size: 16px;
+  font-size: 12px;
   text-align: left;
 `;
 
 export const TableHeader = styled.th`
-  background-color: #f8f8f8;
-  padding: 15px;
-  border-radius: 10px;
+  background-color: #FFFFFF;
+  padding: 0.5rem;
   border-bottom: 2px solid #ddd;
-  text-align: left;
+  text-align: center;
   font-weight: bold;
   color: #333;
-`;
 
-export const TableRow = styled.tr`
-  &:nth-child(even) {
-    background-color: #f9f9f9;
+  &:first-child {
+    border-top-left-radius: 10px;
   }
 
+  &:last-child {
+    border-top-right-radius: 10px;
+  }
+`;
+
+
+export const TableRow = styled.tr`
+  background-color: #FFFFFF;
+  
   &:hover {
     background-color: #f1f1f1;
   }
 `;
 
 export const TableData = styled.td`
-  padding: 15px;
+  padding: .6rem;
   border-bottom: 1px solid #ddd;
   vertical-align: middle;
 `;
@@ -40,9 +46,9 @@ export const GuestContainer = styled.div`
 `;
 
 export const GuestImage = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 8px;
   margin-right: 10px;
 `;
 
@@ -59,25 +65,25 @@ export const GuestInfo = styled.div`
 
 export const StatusBadge = styled.span`
   display: inline-block;
-  padding: 5px 10px;
+  padding: 0.5rem 1rem;
   border-radius: 5px;
-  color: white;
-  font-size: 14px;
-  background-color: ${({ status }) => {
-    switch (status) {
-      case "Booked":
-        return "#28a745";
-      case "Refund":
-        return "#dc3545";
-      case "Pending":
-        return "#ffc107";
-      case "Canceled":
-        return "#6c757d";
-      default:
-        return "#17a2b8";
-    }
-  }};
+  font-size: 12px;
+  text-align: center;
+
+  /* Definimos los colores en un objeto de mapeo */
+  background-color: ${({ status }) => ({
+    "Check-In": "#E8FFEE",
+    "Check-Out": "#E2E2E2",
+    Refund: "#FFEDEC",
+  }[status] || "#17a2b8")}; 
+
+  color: ${({ status }) => ({
+    "Check-In": "#135846",
+    "Check-Out": "#6c757d",
+    Refund: "#dc3545",
+  }[status] || "#ffffff")}; 
 `;
+
 
 export const PaginationContainer = styled.div`
   display: flex;
